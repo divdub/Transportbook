2409f3f7a35ef5ab3597edb16992eedd3c548ff7
Divyansh Dubey <divdub697@gmail.com>
2026-09-03 10:03:00 +0530

    backend

--- transport backend/api.php
+++ transport backend/api.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Api\PartyController;
 use App\Http\Controllers\Api\TruckController;
 use App\Http\Controllers\Api\SupplierController;
 use App\Http\Controllers\Api\TripController;
+use App\Http\Controllers\Api\CityController;
+use App\Http\Controllers\Api\ExpenseController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,3 +22,5 @@ Route::apiResource('parties', PartyController::class);
 Route::apiResource('trucks', TruckController::class);
 Route::apiResource('suppliers', SupplierController::class);
 Route::apiResource('trips', TripController::class);
+Route::apiResource('cities', CityController::class);
+Route::apiResource('expenses', ExpenseController::class);
